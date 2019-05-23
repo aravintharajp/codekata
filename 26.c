09d91144e7d4870b2386533a2958c978b4d46c65
dhanadurai <41063418+dhanadurai@users.noreply.github.com>
2019-05-23 22:01:35 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int n,m[n],i,temp=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&m[i]);
+    }
+   for(i=0;i<n;i++)
+   {
+		for(int j=0;j<n;j++)
+		{
+			if(m[i]<m[j])
+			{
+				temp=m[i];
+				m[i]=m[j];
+				m[j]=temp;
+			}
+		}
+	
+   	}
+   	for (i=0;i<n;i++)
+   	{
+   		printf("%d ",m[i]);
+   	}
+}
